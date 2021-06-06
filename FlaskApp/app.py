8c1e4c9d409d6dd100c0c27d8d0e9081c75d36b8
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-06-06 17:21:29 +0200

    last flask update

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import joblib
 
 model = joblib.load(r"C:\Users\straw\Desktop\AIS\ProjectPool 2\Roue-des-emotions\Output\Models\nlp_models.pkl")
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods = ['POST', 'GET'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        #return render_template('prediction.html')
+        return "<h3>No sailors to display for this query...</h3>"
+    else:
+        return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ FlaskApp/nlp_preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import string
+import numpy as np
+
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+import nltk
+nltk.download("stopwords")
+nltk.download('punkt')
+nltk.download('wordnet')
+
+def NLPprocess(self):
+    def __init__(self):
